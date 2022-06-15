15cada50b6f41ad0a2141bdee0d1a47949ef6f16
Kevin P. O'Brien <kpobrien@mit.edu>
2022-06-15 18:00:56 -0400

    clean up tests file

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,9 +5,6 @@ using Test, Documenter, JosephsonCircuits
 DocMeta.setdocmeta!(JosephsonCircuits, :DocTestSetup, :(using JosephsonCircuits,SparseArrays,BandedMatrices,LinearAlgebra); recursive=true)
 #makedocs(sitename="My Documentation",modules=[QCE])
 
-# one issue with the FFT tests is that they error if the FFTW provider
-# is the default fftw instead of mkl. This can be changed to MKL with the
-# command FFTW.set_provider!("mkl")
-
 doctest(JosephsonCircuits; manual = false)
+
 
