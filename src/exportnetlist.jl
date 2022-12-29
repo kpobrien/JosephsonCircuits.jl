2a344ca2dca45b8400d7f808786684d48183863f
Kevin P. O'Brien <kpobrien@mit.edu>
2022-12-29 00:12:12 -0500

    add docstring for exportnetlist

--- src/exportnetlist.jl
+++ src/exportnetlist.jl
@@ -85,6 +85,32 @@ end
     exportnetlist(circuit::Vector,circuitdefs::Dict,port::Int = true,
         jj::Bool = true)
 
+# Examples
+```jldoctest
+@variables R Cc Lj Cj
+circuit = [
+    ("P1","1","0",1),
+    ("R1","1","0",R),
+    ("C1","1","2",Cc),
+    ("Lj1","2","0",Lj),
+    ("C2","2","0",Cj)]
+
+circuitdefs = Dict(
+    Lj =>1000.0e-12,
+    Cc => 100.0e-15,
+    Cj => 1000.0e-15,
+    R => 50.0)
+
+println(JosephsonCircuits.exportnetlist(circuit, circuitdefs;port = 1, jj = true).netlist)
+
+# output
+* SPICE Simulation
+B1 2 0 3 jjk ics=0.32910597599999997u
+Cj1 2 0 670.8940240000001f
+.model jjk jj(rtype=0,cct=1,icrit=0.32910597599999997u,cap=329.105976f,force=1,vm=9.9
+C1 1 2 100.0f
+R1 1 0 50.0
+```
 """
 function exportnetlist(circuit::Vector,circuitdefs::Dict;port::Int = 1,
         jj::Bool = true)
@@ -357,199 +383,199 @@ function exportnetlist(circuit::Vector,circuitdefs::Dict;port::Int = 1,
     return  (netlist=join(netlist,"\n"),portnodes=portnodes,port=port,portcurrent=portcurrent,Nnodes = Nnodes)
 end
 
-function exportnetlist(cdict::Dict{Any, Any},Nnodes::Int,port::Int = 1,
-    jj::Bool = true)
+# function exportnetlist(cdict::Dict{Any, Any},Nnodes::Int,port::Int = 1,
+#     jj::Bool = true)
 
-    # find the nodes for the selected port
-    # i should also support multiple ports. maybe pass in an array of port indices.
-    portnodes=findall(x->x == port, cdict[:P])
-    portcurrent = 0.0
-    if isempty(portnodes)
-        error("Port $(port) does not exist in dictionary.")
-    else
-        portnodes=first(portnodes)
-        portcurrent=cdict[:I][portnodes]
-    end
+#     # find the nodes for the selected port
+#     # i should also support multiple ports. maybe pass in an array of port indices.
+#     portnodes=findall(x->x == port, cdict[:P])
+#     portcurrent = 0.0
+#     if isempty(portnodes)
+#         error("Port $(port) does not exist in dictionary.")
+#     else
+#         portnodes=first(portnodes)
+#         portcurrent=cdict[:I][portnodes]
+#     end
 
-    # define scale factors for prefixes
-    # multiply by these scale factors
-    femto = 1e15
-    pico = 1e12
-    nano = 1e9
-    micro = 1e6
-    giga = 1e-9
+#     # define scale factors for prefixes
+#     # multiply by these scale factors
+#     femto = 1e15
+#     pico = 1e12
+#     nano = 1e9
+#     micro = 1e6
+#     giga = 1e-9
 
-    # Set vm, (reference icrit)*rsub, which determines the junction resistance
-    # default is 16.5e-3 which is extremely lossy. The allowed range is 8e-3 to 
-    # 100e-3. Once the force flag is enabled we can increase beyond this limit.
-    # To turn off the force flag remove force=1 from the jj model argument 
-    # setting that force=0 does nothing. 
-    # http://www.wrcad.com/ftp/pub/jj.va
-    vm = 99e-1
+#     # Set vm, (reference icrit)*rsub, which determines the junction resistance
+#     # default is 16.5e-3 which is extremely lossy. The allowed range is 8e-3 to 
+#     # 100e-3. Once the force flag is enabled we can increase beyond this limit.
+#     # To turn off the force flag remove force=1 from the jj model argument 
+#     # setting that force=0 does nothing. 
+#     # http://www.wrcad.com/ftp/pub/jj.va
+#     vm = 99e-1
 
-    # define an array of strings for the netlist
-    netlist =  ["* SPICE Simulation"]
+#     # define an array of strings for the netlist
+#     netlist =  ["* SPICE Simulation"]
 
-    Icmean = 0
-    Icmax = 0
-    Icmin = 0
-    Cjmean = 0
+#     Icmean = 0
+#     Icmax = 0
+#     Icmin = 0
+#     Cjmean = 0
 
-    nJJ=0
-    if haskey(cdict,:C)
-        Cdict = copy(cdict[:C])
-    end
-    CjoIc = 0
+#     nJJ=0
+#     if haskey(cdict,:C)
+#         Cdict = copy(cdict[:C])
+#     end
+#     CjoIc = 0
 
-    if haskey(cdict,:Lj)
+#     if haskey(cdict,:Lj)
 
-        for (Ljedge,val) in cdict[:Lj]
-            nJJ+=1
-            Ictmp = LjtoIc(real(cdict[:Lj][Ljedge]))
-            Icmean = Icmean + (Ictmp-Icmean)/nJJ
-            if !haskey(Cdict,Ljedge)
-                error("Error: No junction capacitance found for junction at: $(Ljedge)")
-            end
+#         for (Ljedge,val) in cdict[:Lj]
+#             nJJ+=1
+#             Ictmp = LjtoIc(real(cdict[:Lj][Ljedge]))
+#             Icmean = Icmean + (Ictmp-Icmean)/nJJ
+#             if !haskey(Cdict,Ljedge)
+#                 error("Error: No junction capacitance found for junction at: $(Ljedge)")
+#             end
 
-            CjoIctmp = real(pop!(Cdict,Ljedge)/Ictmp)
-            # println(nJJ)
+#             CjoIctmp = real(pop!(Cdict,Ljedge)/Ictmp)
+#             # println(nJJ)
 
-            if nJJ == 1
-                CjoIc = CjoIctmp
-                Icmin = Ictmp
-            end
+#             if nJJ == 1
+#                 CjoIc = CjoIctmp
+#                 Icmin = Ictmp
+#             end
             
-            if Ictmp < Icmin
-                Icmin = Ictmp
-            end
+#             if Ictmp < Icmin
+#                 Icmin = Ictmp
+#             end
 
-            if Ictmp > Icmax
-                Icmax = Ictmp
-            end
+#             if Ictmp > Icmax
+#                 Icmax = Ictmp
+#             end
 
-            # we can always add a separate junction capacitance so we want to find the minimum
-            # Cj / Ic to use in the jj model. 
-            if CjoIctmp == 0.0
-                error("Cj cannot be zero in the WRSPICE JJ model.")
-            elseif CjoIctmp < CjoIc
-                CjoIc = CjoIctmp
-            end
-
-
-            # if abs((CjoIctmp - CjoIc)/CjoIc) > 1e-8
-            #     error("Cj over critical current ratio not constant. This is required for the WRSPICE JJ model. ")
-            # # idea, i can find the minimum CjoIc ratio, set that equal to the junction capacitance, then
-            # # add additional capacitance. 
-            # else
-            #     CjoIc = CjoIctmp
-            # end
-        end
-        #decide on the JJ parameters and write the junction model.
-        Icmean*CjoIc
-
-        # check if the junction sizes are within the range allowed by WRSPICE
-        if Icmin/Icmean < 0.02
-            error("Minimum junction too much smaller than average for WRSPICE.")
-        end
-        if Icmax/Icmean > 50.0
-            error("Maximum junction too much larger than average for WRSPICE.")
-        end
-
-        # check if the ratio of Cj / Ic is within the range allowed by WRSPICE
-        if CjoIc > 1e-6
-            CjoIc = 1e-6
-        end
-        # if CjoIc > 1e-9
-        #     CjoIc = 1e-9
-        # end
+#             # we can always add a separate junction capacitance so we want to find the minimum
+#             # Cj / Ic to use in the jj model. 
+#             if CjoIctmp == 0.0
+#                 error("Cj cannot be zero in the WRSPICE JJ model.")
+#             elseif CjoIctmp < CjoIc
+#                 CjoIc = CjoIctmp
+#             end
 
 
-        ## write the JJs (or linear inductors in place of JJs) ##
-        inductorlabels = Dict()
-        i = 1
-        for (key,val) in sort(collect(cdict[:Lj]), by=x->x[1][1])
-            Ictmp = LjtoIc(real(val))
-            if jj == true
-                push!(netlist,"B$(i) $(key[1]-1) $(key[2]-1) $(Nnodes+i-1) jjk ics=$(micro*Ictmp)u")
-                # add any additional capacitance
-                if real(cdict[:C][key]) > Ictmp*CjoIc
-                    push!(netlist,"Cj$(i) $(key[1]-1) $(key[2]-1) $(femto*real(cdict[:C][key]-Ictmp*CjoIc))f")
-                end       
-            else
-                push!(netlist,"Lj$(i) $(key[1]-1) $(key[2]-1) $(pico*real(val))p")
-                inductorlabels[(key[1],key[2])] = "Lj$(i)"
-                inductorlabels[(key[2],key[1])] = "Lj$(i)"
-                push!(netlist,"Cj$(i) $(key[1]-1) $(key[2]-1) $(femto*real(cdict[:C][key]))f")
-            end
-            i+=1
-        end
+#             # if abs((CjoIctmp - CjoIc)/CjoIc) > 1e-8
+#             #     error("Cj over critical current ratio not constant. This is required for the WRSPICE JJ model. ")
+#             # # idea, i can find the minimum CjoIc ratio, set that equal to the junction capacitance, then
+#             # # add additional capacitance. 
+#             # else
+#             #     CjoIc = CjoIctmp
+#             # end
+#         end
+#         #decide on the JJ parameters and write the junction model.
+#         Icmean*CjoIc
 
-        if jj == true
-            push!(netlist,".model jjk jj(rtype=0,cct=1,icrit=$(micro*Icmean)u,cap=$(femto*Icmean*real(CjoIc))f,force=1,vm=$(vm)")
-        end
-    end
+#         # check if the junction sizes are within the range allowed by WRSPICE
+#         if Icmin/Icmean < 0.02
+#             error("Minimum junction too much smaller than average for WRSPICE.")
+#         end
+#         if Icmax/Icmean > 50.0
+#             error("Maximum junction too much larger than average for WRSPICE.")
+#         end
 
-    if haskey(cdict,:L)
-        ## write the linear inductors ##
-        i = 1
-        for (key,val) in sort(collect(cdict[:L]), by=x->x[1][1])
-            push!(netlist,"L$(i) $(key[1]-1) $(key[2]-1) $(pico*real(val))p")
-            inductorlabels[(key[1],key[2])] = "L$(i)"
-            inductorlabels[(key[2],key[1])] = "L$(i)"
-            i+=1
-        end
-    end
+#         # check if the ratio of Cj / Ic is within the range allowed by WRSPICE
+#         if CjoIc > 1e-6
+#             CjoIc = 1e-6
+#         end
+#         # if CjoIc > 1e-9
+#         #     CjoIc = 1e-9
+#         # end
 
-    if haskey(cdict,:Lm)
-        ## write the mutual inductors ##
-        # loop over the mutual inductors 
-        i = 1
-        for (key,val) in sort(collect(cdict[:Lm]), by=x->x[1][1])
-            L1label = inductorlabels[(key[1],key[2])]
-            L2label = inductorlabels[(key[3],key[4])]
-            L1 = cdict[:L][(key[1],key[2])]
-            L2 = cdict[:L][(key[3],key[4])]
 
-            K = real(val/sqrt(L1*L2))
-            push!(netlist,"K$(i) $(L1label) $(L2label) $(K)")
-            i+=1
-        end
-    end
+#         ## write the JJs (or linear inductors in place of JJs) ##
+#         inductorlabels = Dict()
+#         i = 1
+#         for (key,val) in sort(collect(cdict[:Lj]), by=x->x[1][1])
+#             Ictmp = LjtoIc(real(val))
+#             if jj == true
+#                 push!(netlist,"B$(i) $(key[1]-1) $(key[2]-1) $(Nnodes+i-1) jjk ics=$(micro*Ictmp)u")
+#                 # add any additional capacitance
+#                 if real(cdict[:C][key]) > Ictmp*CjoIc
+#                     push!(netlist,"Cj$(i) $(key[1]-1) $(key[2]-1) $(femto*real(cdict[:C][key]-Ictmp*CjoIc))f")
+#                 end       
+#             else
+#                 push!(netlist,"Lj$(i) $(key[1]-1) $(key[2]-1) $(pico*real(val))p")
+#                 inductorlabels[(key[1],key[2])] = "Lj$(i)"
+#                 inductorlabels[(key[2],key[1])] = "Lj$(i)"
+#                 push!(netlist,"Cj$(i) $(key[1]-1) $(key[2]-1) $(femto*real(cdict[:C][key]))f")
+#             end
+#             i+=1
+#         end
 
-    if haskey(cdict,:C)
-        ## write the capacitors ##
-        i = 1
-        for (key,val) in sort(collect(Cdict), by=x->x[1][1])
-            push!(netlist,"C$(i) $(key[1]-1) $(key[2]-1) $(femto*real(val))f")
-            i+=1
-        end
-    end
+#         if jj == true
+#             push!(netlist,".model jjk jj(rtype=0,cct=1,icrit=$(micro*Icmean)u,cap=$(femto*Icmean*real(CjoIc))f,force=1,vm=$(vm)")
+#         end
+#     end
 
-    ## write the resistors ##
-    if haskey(cdict,:R)
-        i = 1
-        for (key,val) in sort(collect(cdict[:R]), by=x->x[1][1])
-            push!(netlist,"R$(i) $(key[1]-1) $(key[2]-1) $(real(val))")
-            i+=1
-        end
-    end
+#     if haskey(cdict,:L)
+#         ## write the linear inductors ##
+#         i = 1
+#         for (key,val) in sort(collect(cdict[:L]), by=x->x[1][1])
+#             push!(netlist,"L$(i) $(key[1]-1) $(key[2]-1) $(pico*real(val))p")
+#             inductorlabels[(key[1],key[2])] = "L$(i)"
+#             inductorlabels[(key[2],key[1])] = "L$(i)"
+#             i+=1
+#         end
+#     end
 
-    if haskey(cdict,:V)
-        ## write the voltage sources ##
-        i = 1
-        for (key,val) in sort(collect(cdict[:V]), by=x->x[1][1])
-            push!(netlist,"V$(i) $(key[1]-1) $(key[2]-1) $(real(val))")
-            i+=1
-        end
-    end
+#     if haskey(cdict,:Lm)
+#         ## write the mutual inductors ##
+#         # loop over the mutual inductors 
+#         i = 1
+#         for (key,val) in sort(collect(cdict[:Lm]), by=x->x[1][1])
+#             L1label = inductorlabels[(key[1],key[2])]
+#             L2label = inductorlabels[(key[3],key[4])]
+#             L1 = cdict[:L][(key[1],key[2])]
+#             L2 = cdict[:L][(key[3],key[4])]
 
-    if haskey(cdict,:I)
-        ## write the current sources ##
-        for (key,val) in sort(collect(cdict[:I]), by=x->x[1][1])
-            #push!(netlist,"I$(i) $(key[1]-1) $(key[2]-1) $(real(val))")
-            i+=1
-        end
-    end
+#             K = real(val/sqrt(L1*L2))
+#             push!(netlist,"K$(i) $(L1label) $(L2label) $(K)")
+#             i+=1
+#         end
+#     end
 
-    return (netlist=join(netlist,"\n"),portnodes=portnodes,port=port,portcurrent=portcurrent)
-end
+#     if haskey(cdict,:C)
+#         ## write the capacitors ##
+#         i = 1
+#         for (key,val) in sort(collect(Cdict), by=x->x[1][1])
+#             push!(netlist,"C$(i) $(key[1]-1) $(key[2]-1) $(femto*real(val))f")
+#             i+=1
+#         end
+#     end
+
+#     ## write the resistors ##
+#     if haskey(cdict,:R)
+#         i = 1
+#         for (key,val) in sort(collect(cdict[:R]), by=x->x[1][1])
+#             push!(netlist,"R$(i) $(key[1]-1) $(key[2]-1) $(real(val))")
+#             i+=1
+#         end
+#     end
+
+#     if haskey(cdict,:V)
+#         ## write the voltage sources ##
+#         i = 1
+#         for (key,val) in sort(collect(cdict[:V]), by=x->x[1][1])
+#             push!(netlist,"V$(i) $(key[1]-1) $(key[2]-1) $(real(val))")
+#             i+=1
+#         end
+#     end
+
+#     if haskey(cdict,:I)
+#         ## write the current sources ##
+#         for (key,val) in sort(collect(cdict[:I]), by=x->x[1][1])
+#             #push!(netlist,"I$(i) $(key[1]-1) $(key[2]-1) $(real(val))")
+#             i+=1
+#         end
+#     end
+
+#     return (netlist=join(netlist,"\n"),portnodes=portnodes,port=port,portcurrent=portcurrent)
+# end
