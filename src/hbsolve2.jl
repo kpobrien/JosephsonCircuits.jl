8fcd037eb9e24ed774031e6b2b21e8e9d62382d9
Kevin P. O'Brien <kpobrien@mit.edu>
2023-02-12 11:32:42 -0500

    add docs and tests for hblinsolve2 and hbnlsolve2

--- src/hbsolve2.jl
+++ src/hbsolve2.jl
@@ -1,12 +1,34 @@
 
 
 """
-    hbsolve2()
+    hbsolve2(ws, wp, Ip, Nmodulationharmonics, Npumpmodes, circuit, circuitdefs;
+        pumpports = [1], solver = :klu, iterations = 1000, ftol = 1e-8,
+        switchofflinesearchtol = 1e-5, alphamin = 1e-4,
+        symfreqvar = nothing, nbatches = Base.Threads.nthreads(), sorting = :number,
+        returnS = true, returnSnoise = false, returnQE = true, returnCM = true,
+        returnnodeflux = false, returnvoltage = false, returnnodefluxadjoint = false,
+        verbosity = 1)
 
-New version of the harmonic balance solver suitable for arbitrary numbers of 
-ports, sources, and pumps. Still under development. 
+Calls the new harmonic balance solvers (which work for an arbitrary number of
+modes and ports) `hbnlsolve2` and `hblinsolve2` using an identical syntax to
+`hbsolve`, which only supports four wave mixing processes involving single
+strong tone and an arbitrary number of tone in the linearized solver. This
+function is primarily for testing the new solvers and will eventually be
+deprecated. 
+
+This function attempts to mimic `hbsolve`, but has a few important differences:
+1. `Nmodulationharmonics` is no longer the number of signal modes but the number
+    of modulation harmonics meaning `0` would give the signal only, `1` would
+    three wave mixing processes, which this function does not enable. `2` would
+    create idlers on either side of the signal mode at ws+2*wp and ws-2*wp.
+    For other sets of idlers or three wave mixing processes, please use the new
+    interface to `hbsolve2`.
+2. The outputs of the linearized harmonic balance solver `hblinsolve2` may not
+    have the same ordering of signal modes as in `hblinsolve`. In `hblinsolve2`
+    the signal mode is always at index 1 and the location of the other modes
+    can be found by inspecting the contents of `modes`.
 """
-function hbsolve2(ws, wp, Ip, Nsignalmodes, Npumpmodes, circuit, circuitdefs;
+function hbsolve2(ws, wp, Ip, Nmodulationharmonics, Npumpmodes, circuit, circuitdefs;
     pumpports = [1], solver = :klu, iterations = 1000, ftol = 1e-8,
     switchofflinesearchtol = 1e-5, alphamin = 1e-4,
     symfreqvar = nothing, nbatches = Base.Threads.nthreads(), sorting = :number,
@@ -49,7 +71,7 @@ function hbsolve2(ws, wp, Ip, Nsignalmodes, Npumpmodes, circuit, circuitdefs;
 
     # generate the signal modes
     signalfreq =truncfreqs(
-        calcfreqsdft((Nsignalmodes,)),
+        calcfreqsdft((Nmodulationharmonics,)),
         dc=true,odd=false,even=true,maxintermodorder=Inf,
     )
 
@@ -67,12 +89,17 @@ end
 
 
 """
-hblinsolve2(ws, wp, Npumpharmonics, Nmodulationharmonics, psc, cg,
-    circuitdefs, pump; symfreqvar=nothing, threewavemixing=false,
-    fourwavemixing=true)
+    hblinsolve2(w, circuit,circuitdefs; Nmodulationharmonics = (0,),
+        pump=nothing, symfreqvar=nothing, threewavemixing=false,
+        fourwavemixing=true, maxintermodorder=Inf,
+        nbatches::Integer = Base.Threads.nthreads(), returnS = true,
+        returnSnoise = false, returnQE = true, returnCM = true,
+        returnnodeflux = false, returnnodefluxadjoint = false, returnvoltage = false,
+        verbosity = 1)
 
-Experimental single frequency version of harmonic balance solver for linearized
-system.
+Harmonic balance solver supporting an arbitrary number of small signals (weak
+tones) linearized around `pump`, the solution of the nonlinear system consisting
+of an arbitrary number of large signals (strong tones).
 
 # Examples
 ```jldoctest
@@ -156,12 +183,86 @@ return hblinsolve2(w, psc, cg, circuitdefs, signalfreq; pump = pump,
         returnvoltage = returnvoltage)
 end
 
+"""
+    hblinsolve2(w, psc::ParsedSortedCircuit,
+        cg::CircuitGraph, circuitdefs, signalfreq::Frequencies{N}; pump=nothing,
+        symfreqvar=nothing, nbatches::Integer = Base.Threads.nthreads(),
+        returnS = true, returnSnoise = false, returnQE = true, returnCM = true,
+        returnnodeflux = false, returnnodefluxadjoint = false, returnvoltage = false,
+        verbosity = 1)
 
-function hblinsolve2(w, psc, cg, circuitdefs, signalfreq; pump=nothing,
+Harmonic balance solver supporting an arbitrary number of small signals (weak
+tones) linearized around `pump`, the solution of the nonlinear system consisting
+of an arbitrary number of large signals (strong tones).
+
+# Examples
+```jldoctest
+circuit = Array{Tuple{String,String,String,Union{Complex{Float64},Symbol,Int64}},1}(undef,0)
+push!(circuit,("P1","1","0",1))
+push!(circuit,("R1","1","0",:Rleft))
+push!(circuit,("L1","1","0",:Lm)) 
+push!(circuit,("K1","L1","L2",:K1))
+push!(circuit,("C1","1","2",:Cc)) 
+push!(circuit,("L2","2","3",:Lm)) 
+push!(circuit,("Lj3","3","0",:Lj)) 
+push!(circuit,("Lj4","2","0",:Lj)) 
+push!(circuit,("C2","2","0",:Cj))
+circuitdefs = Dict{Symbol,Complex{Float64}}(
+    :Lj =>2000e-12,
+    :Lm =>10e-12,
+    :Cc => 200.0e-15,
+    :Cj => 900e-15,
+    :Rleft => 50.0,
+    :Rright => 50.0,
+    :K1 => 0.9,
+)
+
+Idc = 1e-6*0
+Ip=5.0e-6
+wp=2*pi*5e9
+ws=2*pi*5.2e9
+Npumpharmonics = (2,)
+Nmodulationharmonics = (2,)
+threewavemixing=false
+fourwavemixing=true
+
+frequencies = JosephsonCircuits.removeconjfreqs(
+    JosephsonCircuits.truncfreqs(
+        JosephsonCircuits.calcfreqsrdft(Npumpharmonics),
+        dc=true, odd=true, even=false, maxintermodorder=Inf,
+    )
+)
+fi = JosephsonCircuits.fourierindices(frequencies)
+Nmodes = length(frequencies.modes)
+psc = JosephsonCircuits.parsesortcircuit(circuit)
+cg = JosephsonCircuits.calccircuitgraph(psc)
+nm = JosephsonCircuits.numericmatrices(psc, cg, circuitdefs, Nmodes = Nmodes)
+pump=hbnlsolve2(
+    (wp,),
+    [
+        (mode=(0,),port=1,current=Idc),
+        (mode=(1,),port=1,current=Ip),
+    ],
+    frequencies, fi, psc, cg, nm)
+signalfreq =JosephsonCircuits.truncfreqs(
+    JosephsonCircuits.calcfreqsdft(Nmodulationharmonics),
+    dc=true,odd=threewavemixing,even=fourwavemixing,maxintermodorder=Inf,
+)
+signal = JosephsonCircuits.hblinsolve2(ws, psc, cg, circuitdefs, signalfreq;pump=pump,returnnodeflux=true)
+isapprox(signal.nodeflux,
+    ComplexF64[9.901008591291e-12 - 6.40587007644028e-14im 2.164688307719963e-14 - 2.90852607344097e-16im 6.671563044645655e-14 - 8.585524364135119e-16im; 2.1633104519765224e-14 - 8.251861334047893e-16im 1.0099063486905209e-11 - 1.948847859339803e-13im -8.532003011745068e-15 + 3.234788465760295e-16im; 6.671648606599472e-14 + 7.892709980649199e-16im -8.53757633177974e-15 - 9.748395563374129e-17im 9.856580758892428e-12 + 5.859984004390703e-14im; 1.5888896262186103e-11 - 1.0303480614499543e-13im -2.557126237504446e-12 + 1.759201163407723e-14im -8.475819811683215e-12 + 5.3531443609574795e-14im; -2.5781681021577177e-13 + 4.757590640631487e-15im 2.36818731889176e-12 - 4.569646499606389e-14im 1.116372367616482e-13 - 2.039935997276492e-15im; -1.0210743447568219e-11 - 5.905490368441375e-14im 1.3377918536056493e-12 + 7.190105205618706e-15im 2.5392856657302323e-11 + 1.5143842454586225e-13im; 2.4781693042536835e-11 - 1.6057018472176702e-13im -2.5342360504077476e-12 + 1.7306764301173096e-14im -8.40554044664581e-12 + 5.269404591748149e-14im; -2.348528974341763e-13 + 3.949450668269274e-15im 1.1449271118157543e-11 - 2.2093702114766968e-13im 1.0261871618968225e-13 - 1.7240213938923877e-15im; -1.0140560031409567e-11 - 5.828587508192886e-14im 1.3288225860409326e-12 + 7.0954601524623594e-15im 3.423954321087654e-11 + 2.0403371894291513e-13im],
+    atol = 1e-6)
+
+# output
+true
+```
+"""
+function hblinsolve2(w, psc::ParsedSortedCircuit,
+    cg::CircuitGraph, circuitdefs, signalfreq::Frequencies{N}; pump=nothing,
     symfreqvar=nothing, nbatches::Integer = Base.Threads.nthreads(),
     returnS = true, returnSnoise = false, returnQE = true, returnCM = true,
     returnnodeflux = false, returnnodefluxadjoint = false, returnvoltage = false,
-    verbosity = 1)
+    verbosity = 1) where N
 
     Nsignalmodes = length(signalfreq.modes)
     # calculate the numeric matrices
@@ -392,9 +493,10 @@ function hblinsolve2(w, psc, cg, circuitdefs, signalfreq; pump=nothing,
 end
 
 """
-    hbnlsolve2(w::NTuple{N,Any}, Nharmonics::NTuple{N,Int}, sources, circuit,
-        circuitdefs; solver = :klu, iterations = 1000, maxintermodorder = Inf,
-        dc = false, odd = true, even = false, x0 = nothing, ftol = 1e-8,
+    hbnlsolve2(w::NTuple{N,Any}, Nharmonics::NTuple{N,Int}, sources,
+        circuit, circuitdefs; solver = :klu, iterations = 1000,
+        maxintermodorder = Inf, dc = false, odd = true, even = false, x0 = nothing,
+        ftol = 1e-8, switchofflinesearchtol = 1e-5, alphamin = 1e-4,
         symfreqvar = nothing, sorting= :number)
 
 New version of the nonlinear harmonic balance solver suitable for arbitrary
@@ -405,7 +507,6 @@ or flux pumping using a current source and a mutual inductor.
 ```jldoctest
 circuit = Array{Tuple{String,String,String,Union{Complex{Float64},Symbol,Int64}},1}(undef,0)
 push!(circuit,("P1","1","0",1))
-push!(circuit,("I1","1","0",:Ipump))
 push!(circuit,("R1","1","0",:Rleft))
 push!(circuit,("L1","1","0",:Lm)) 
 push!(circuit,("K1","L1","L2",:K1))
@@ -421,7 +522,6 @@ circuitdefs = Dict{Symbol,Complex{Float64}}(
     :Cj => 900e-15,
     :Rleft => 50.0,
     :Rright => 50.0,
-    :Ipump => 1.0e-8,
     :K1 => 0.9,
 )
 
@@ -445,13 +545,10 @@ isapprox(out.nodeflux,
 true
 ```
 """
-function hbnlsolve2(
-    w::NTuple{N,Any},
-    Nharmonics::NTuple{N,Int},
-    sources,
-    circuit, circuitdefs; solver = :klu, iterations = 1000, maxintermodorder = Inf,
-    dc = false, odd = true, even = false, x0 = nothing, ftol = 1e-8,
-    switchofflinesearchtol = 1e-5, alphamin = 1e-4,
+function hbnlsolve2(w::NTuple{N,Any}, Nharmonics::NTuple{N,Int}, sources,
+    circuit, circuitdefs; solver = :klu, iterations = 1000,
+    maxintermodorder = Inf, dc = false, odd = true, even = false, x0 = nothing,
+    ftol = 1e-8, switchofflinesearchtol = 1e-5, alphamin = 1e-4,
     symfreqvar = nothing, sorting= :number) where N
 
     # calculate the frequency struct
@@ -481,10 +578,71 @@ function hbnlsolve2(
         symfreqvar = symfreqvar)
 end
 
-function hbnlsolve2(
-    w::NTuple{N,Any},
-    sources,
-    frequencies::Frequencies{N},
+"""
+    hbnlsolve2(w::NTuple{N,Any}, sources, frequencies::Frequencies{N},
+        indices::FourierIndices{N}, psc::ParsedSortedCircuit, cg::CircuitGraph,
+        nm::CircuitMatrices; solver = :klu, iterations = 1000, x0 = nothing,
+        ftol = 1e-8, switchofflinesearchtol = 1e-5, alphamin = 1e-4,
+        symfreqvar = nothing)
+
+New version of the nonlinear harmonic balance solver suitable for arbitrary
+numbers of ports, sources, and drives including direct current (zero frequency)
+or flux pumping using a current source and a mutual inductor.
+
+# Examples
+```jldoctest
+circuit = Array{Tuple{String,String,String,Union{Complex{Float64},Symbol,Int64}},1}(undef,0)
+push!(circuit,("P1","1","0",1))
+push!(circuit,("R1","1","0",:Rleft))
+push!(circuit,("L1","1","0",:Lm)) 
+push!(circuit,("K1","L1","L2",:K1))
+push!(circuit,("C1","1","2",:Cc)) 
+push!(circuit,("L2","2","3",:Lm)) 
+push!(circuit,("Lj3","3","0",:Lj)) 
+push!(circuit,("Lj4","2","0",:Lj)) 
+push!(circuit,("C2","2","0",:Cj))
+circuitdefs = Dict{Symbol,Complex{Float64}}(
+    :Lj =>2000e-12,
+    :Lm =>10e-12,
+    :Cc => 200.0e-15,
+    :Cj => 900e-15,
+    :Rleft => 50.0,
+    :Rright => 50.0,
+    :K1 => 0.9,
+)
+
+Idc = 50e-5
+Ip=0.0001e-6
+wp=2*pi*5e9
+Nharmonics = (2,)
+frequencies = JosephsonCircuits.removeconjfreqs(
+    JosephsonCircuits.truncfreqs(
+        JosephsonCircuits.calcfreqsrdft(Nharmonics),
+        dc=true, odd=true, even=false, maxintermodorder=Inf,
+    )
+)
+fi = JosephsonCircuits.fourierindices(frequencies)
+Nmodes = length(frequencies.modes)
+psc = JosephsonCircuits.parsesortcircuit(circuit)
+cg = JosephsonCircuits.calccircuitgraph(psc)
+nm = JosephsonCircuits.numericmatrices(psc, cg, circuitdefs, Nmodes = Nmodes)
+
+out=hbnlsolve2(
+    (wp,),
+    [
+        (mode=(0,),port=1,current=Idc),
+        (mode=(1,),port=1,current=Ip),
+    ],
+    frequencies, fi, psc, cg, nm)
+isapprox(out.nodeflux,
+    ComplexF64[15.190314040027522 - 8.56492651167657e-24im, 2.991103820177504e-6 - 1.8501001011477133e-8im, -6.835392148510984 - 1.0356102442254259e-14im, 7.396422335315908e-6 - 4.5749403967992827e-8im, 6.835392148539885 - 1.0356102451770844e-14im, 1.008026285172782e-5 - 6.23498762664213e-8im],
+    atol = 1e-6)
+
+# output
+true
+```
+"""
+function hbnlsolve2(w::NTuple{N,Any}, sources, frequencies::Frequencies{N},
     indices::FourierIndices{N}, psc::ParsedSortedCircuit, cg::CircuitGraph,
     nm::CircuitMatrices; solver = :klu, iterations = 1000, x0 = nothing,
     ftol = 1e-8, switchofflinesearchtol = 1e-5, alphamin = 1e-4,
