71a67092a9bb691173db678df428be416efbb12a
Kevin P. O'Brien <kpobrien@mit.edu>
2023-09-17 15:56:11 -0400

    attempt to add documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,8 @@
 using Test, Documenter, JosephsonCircuits
 
 DocMeta.setdocmeta!(JosephsonCircuits, :DocTestSetup, :(using JosephsonCircuits); recursive=true)
-#makedocs(sitename="My Documentation",modules=[JosephsonCircuits])
-#doctest(QCE; manual = false)
-makedocs(sitename="JosephsonCircuits")
+makedocs(modules = [JosephsonCircuits], sitename="JosephsonCircuits.jl")
 
-#using Documenter
-#using JosephsonCircuits
-
-#DocMeta.setdocmeta!(MyPackage, :DocTestSetup, :(using JosephsonCircuits); recursive=true)
-#makedocs(modules=[MyPackage], ...)
-
-#makedocs(sitename="My Documentation")
+deploydocs(
+   repo = "github.com/kpobrien/JosephsonCircuits.jl.git",
+)
