61cde701ce572a1fd3b60386d8a631cb440d4e99
Kevin P. O'Brien <kpobrien@mit.edu>
2026-05-25 10:32:38 -0400

    add another test for unwrap

--- test/unwrap.jl
+++ test/unwrap.jl
@@ -5,6 +5,14 @@ using JosephsonCircuits
 
     @testset "unwrap!" begin
 
+        theta=0:0.01:6*pi
+        @test isapprox(theta,JosephsonCircuits.unwrap(angle.(exp.(im*theta))))
+
+        @test isapprox(
+            JosephsonCircuits.unwrap!(zeros(10,10);dims=1),
+            zeros(10,10),
+        )
+
         @test_throws(
             ArgumentError(lazy"`unwrap!`: required keyword parameter dims missing"),
             JosephsonCircuits.unwrap!(zeros(10,10),zeros(10,10)),
