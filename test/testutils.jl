a7ace1662527eb4f4e87b274ac946397b2abd02a
Kevin P. O'Brien <kpobrien@mit.edu>
2026-05-25 10:53:40 -0400

    add compare test

--- test/testutils.jl
+++ test/testutils.jl
@@ -3,5 +3,12 @@ using Test
 
 @testset verbose=true "testutils" begin
 
+    @testset "compare" begin
 
+        @test JosephsonCircuits.compare(
+            JosephsonCircuits.PassiveNetwork("S1",zeros(2,2),zeros(2,2),[("S1",1),("S1",2)]),
+            JosephsonCircuits.PassiveNetwork("S1",zeros(2,2),zeros(2,2),[("S1",1),("S1",2)]),
+        )
+
+    end
 end
